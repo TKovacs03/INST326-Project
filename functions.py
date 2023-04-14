243f3a14efedab668b9416d2ff5d3d3427ff83dc
Alex Hildebrand <alexmhildebrand@gmail.com>
2023-04-14 19:53:50 -0400

    added a calorie tracker function

--- functions.py
+++ functions.py
@@ -106,6 +106,44 @@ def total_cal_intake(bmr, active_level):
     else:
         daily_cals = (bmr * 1.9)
         return daily_cals
+#Alex Hildebrand Function   
+foodcals = {}
+def calorie_tracker(food, calories, done = False, goal = 2000):
+    """Allows you to see the foods you've eaten and their calories and updates you on where you
+    are at in reaching your calorie goal
+    Args:
+        food(str): What food you ate
+        calories(int): the calorie count for the corresponding food
+        goal(int, optional) = The amount of calories you want to have for the day, default is 2000
+        done(boolean, optional): Whether you are done eating for the day, default is False meaning not 
+        done
+    Returns: String of the foods and calories in the dictionary and how many more calories you need
+    to hit your goal
+    Side Effects: Prints to the console and changes global variable"""
+    foodcals[food] = calories
+    total_calories = sum(foodcals.values())
+    if total_calories < goal:
+        for key in foodcals:
+            print(f"{key}: {foodcals[key]} calories")
+        print(f"You still need to eat {goal - total_calories} calories")
+    if done == True:
+        for key in foodcals:
+            print(f"{key}: {foodcals[key]} calories")
+        print(f"You were {goal - total_calories} calories short")
+        for key in foodcals:
+            foodcals.pop()
+    if goal <= total_calories <= goal + 200:
+        for key in foodcals:
+            print(f"{key}: {foodcals[key]} calories")
+        print(f"You roughly ate your specified calorie count goal!")
+        foodcals.clear()
+    if goal +200 < total_calories:
+        for key in foodcals:
+            print(f"{key}: {foodcals[key]} calories")
+        print("You may have overeaten your calorie goal")
+        foodcals.clear
+    
+
 
         
 
