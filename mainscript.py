e7b2a1cf2ddd98737a5d678e682ec105ed316d6a
TJ Kovacs <tkovacs@terpmail.umd.edu>
2023-05-01 10:52:50 -0400

    edited main script

--- mainscript.py
+++ mainscript.py
@@ -15,4 +15,13 @@ def main(save = None):
         gender = input('Gender?\n')
         level = input('Level of exercise experience (novice, intermediate, expert)\n')
         main_user = user.User(name, age, height, weight, gender, level)
-        
+    choice = input('What would you like to do? (workout, view history, track calories, or save)')
+    if choice == 'workout':
+        workout_type = input('What kind of workout? (push, pull, or legs)')
+        functions.workout_generator(workout_type)
+    elif choice == 'track calories':
+        pass
+    elif choice == 'view history':
+        pass
+    elif choice == 'save':
+        pass
