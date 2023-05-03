6e7521277464a456f492c0ecefbb2aee59217176
Alex Hildebrand <alexmhildebrand@gmail.com>
2023-05-03 18:47:20 -0400

    Added a level input

--- mainscript.py
+++ mainscript.py
@@ -18,11 +18,12 @@ def main(user):
 
         elif choice == 'track calories':
             bmr = functions.BMR(user.gender, user.height, user.weight, user.age)
-            calgoals = functions.total_cal_intake(bmr, user.level)
+            alevel = input("How often do you exercise on a weekly basis(Little to None, Lightly Active,\n Moderately Active, Very Active, Extremely Active)?\n")
+            calgoals = functions.total_cal_intake(bmr, alevel)
             foodcals = {}
-            food = input("What food did you eat?")
-            calories = input("How many calories was it?")
-            doneornot = input("Are you done eating for the day(True/False)?")
+            food = input("What food did you eat?\n")
+            calories = input("How many calories was it?\n")
+            doneornot = input("Are you done eating for the day(True/False)?\n")
             functions.calorie_tracker(food, calories, calgoals, doneornot)
         elif choice == 'bmr':
             bmr = functions.BMR(user.gender, user.height, user.weight, user.age)
